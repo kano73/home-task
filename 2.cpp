d554cc56501dcadfb2694fb8494775ba1641e2bb
kano73 <pasha.nenahov@gmail.com>
2021-09-15 22:39:27 +0300

    home task
    home task

---
+++ 2.cpp
@@ -0,0 +1,28 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c, rez;
+	cout << "-Введите трехзначное число:" << endl;
+	cin >> rez;
+	if (rez % 2 == 0)
+	{
+		cout << "-Это четное число: " << endl;
+	}
+	else
+	{
+		cout << "-Это нечетное число: " << endl;
+	}
+
+	a = rez % 10;
+	b = rez / 10 % 10;
+	c = rez / 100;
+	cout << (a + b + c) / 3.0 << endl;
+
+	system("pause");
+	return 0;
+}
